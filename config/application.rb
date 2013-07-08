76bd90e00f0cd6b0db360b4bfe8bfb1b16b22b5d
de1mos <de1m0s242@gmail.com>
2013-07-09 04:43:46 +0800

    heroku ready

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module De1mosNotes
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Fix for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
