4f3ca7f948aa7b55264e74aa7f08b5ea80d78504
de1mos <de1m0s242@gmail.com>
2013-07-09 03:41:26 +0800

    second try

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "De1mosNotes"
+set :repository,  "https://github.com/de1mos242/De1mosNotes.git"
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "de1mos.net"                          # Your HTTP server, Apache/etc
+role :app, "de1mos.net"                          # This may be the same as your `Web` server
+role :db,  "de1mos.net", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+De1mosNotes::Application.config.secret_token = '1a8fe5eaed54cbbeea131298c3dc60891d20cf8e802e2c2a6eb957b2bd97eea6a25cbda30d0e5716b57b53c97f84d7ace6240fe29725fe8cd70887479a78d682'
